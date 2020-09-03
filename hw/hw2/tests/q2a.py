8323751e897d5ce52086cd17d3a1c0749e86cb8c
David Yi <davidyi624@berkeley.edu>
2020-09-02 23:16:07 -0700

    releasing hw2

---
+++ hw/hw2/tests/q2a.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q2a',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> prob_at_most(3, 0.4, 1) >= 0\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> prob_at_most(5, 0.6, 3) <= 1\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> prob_at_most(2, 3, 4) == 0\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
