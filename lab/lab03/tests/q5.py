811a9409f45affa62039ea8b1062f455568f70e3
dorx <doris.s.xin@gmail.com>
2020-09-11 11:20:38 -0700

    lab03 release

---
+++ lab/lab03/tests/q5.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q5',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> len(result)\n11', 'hidden': False, 'locked': False},
+                                   {'code': '>>> result["Count"].sum()\n38997', 'hidden': False, 'locked': False},
+                                   {'code': '>>> result["Count"].iloc[0]\n4341', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
