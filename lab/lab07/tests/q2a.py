7d11d4b390daac2994a3d9e1f3a39a7bd64eeda6
dorx <doris.s.xin@gmail.com>
2020-10-08 15:03:13 -0700

    [Lab 7] release

---
+++ lab/lab07/tests/q2a.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q2a',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> np.isclose(l2_tip_risk(0, 0), 10.896283606557375)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(l2_tip_risk(0.92026961355467352, 0.10502451738435334), 1.0360194420113771)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab07/tests/q5a.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q5a',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': ">>> set(desired_columns) == set(['horsepower', 'hp^2', 'model_year', 'acceleration'])\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
