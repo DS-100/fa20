4f751910453e706338978ebff2f4f55245678377
David Yi <davidyi624@berkeley.edu>
2020-11-13 11:05:16 -0800

    [Project 2] Public Release

---
+++ proj/proj2/tests/q1a.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q1a',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> original_training_data.isnull().sum().sum() == 0\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
