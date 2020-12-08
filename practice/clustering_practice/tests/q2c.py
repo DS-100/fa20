5db0685834c4c3a049dfbb41c84bfafd1c72d010
David Yi <davidyi624@berkeley.edu>
2020-12-08 10:47:49 -0800

    Adding practice assignments under new practice directory

---
+++ practice/clustering_practice/tests/q2c.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2c',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> sorted([np.count_nonzero(aggc_q2c.labels_ == 0), np.count_nonzero(aggc_q2c.labels_ == 1)]) == [25, 475]\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
